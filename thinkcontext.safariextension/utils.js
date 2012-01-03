06b852af8416e207387eea8e418c172efd06e0e6
unknown <abushnell@.(none)>
2012-01-03 18:33:01 -0500

    minor packaging update

--- thinkcontext.safariextension/utils.js
+++ thinkcontext.safariextension/utils.js
@@ -196,6 +196,7 @@ tc.sigURL = function(url){
 	      || ret.match('http://([\w\.]*\.)?defensenews\.com/[^"?]+')
 	      || ret.match('http://([\w\.]*\.)?msmagazine\.com/news/[^"?]+')
 	      || ret.match('http://([\w\.]*\.)?unep\.org/[^"?]+')
+	      || ret.match('http://([\w\.]*\.)?lamag\.com/[^"?]+')
 	     ){
 	ret = ret.split('#')[0];	      
     } else if(ret.match(/(\w*\.)?cbc.ca\/video/)
