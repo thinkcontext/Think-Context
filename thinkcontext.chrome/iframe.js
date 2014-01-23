11a18e96bc49e648e554060cff9db59ebcaae5d0
thinkContext <contact@thinkcontext.org>
2014-01-23 18:22:34 -0500

    imgad none

--- thinkcontext.chrome/iframe.js
+++ thinkcontext.chrome/iframe.js
@@ -20,23 +20,34 @@ if(document.domain.match('adsonar.com')){
 	function(){
 	    tc.iframe.sendReq(this);
 	});
+
+    // $("td[onmouseover|='wMes']").not('a[sid]').map(
+    // 	function(){
+    // 	    console.log('onmouseover',this.onmouseover);
+    // 	});
 } else if(document.domain.match('msn.com')){
     $('a.AdDisplayUrl').not('a[sid]').map(
 	function(){
 	    tc.iframe.sendReq(this);
 	});
 // } else if(document.baseURI.match('ad.doubleclick.net/adi')){
+//     console.log('doubleclick adi iframe');
 //     $("object param[value*='adurl%3D']").map(
 // 	function(){
+// 	    console.log(this);
 // 	    var m = this.value.match(/sscs%253D%253fhttp(s)?%3A\/\/([^\/]+)/);
 // 	    if(m && m.length == 3){
 // 		console.log('doubleclick param ' + m[2])
 // 	    } else {
-// 		m = this.value.match(/adurl%3Dhttp(s)?%253a%252f%252f([^\/]+)\//)
-// 		if(m && m.length == 3){
-// 		console.log('doubleclick param ' + m[2])
+// 		m = this.value.match(/adurl%253(http[^\&]+)/)
+// 		if(m && m.length == 2){
+// 		console.log('doubleclick param ' + m[1])
 // 		}
 // 	    }
+// 	});
+//     $("a:has(img)").map(
+// 	function(){
+// 	    console.log(this);
 // 	});
 } else if(document.baseURI.match('doubleclick.net/pagead')){
     $("span.adus").not('a[sid]').map(
@@ -48,5 +59,33 @@ if(document.domain.match('adsonar.com')){
 	function(){
 	    tc.iframe.sendReq(this);
 	});
-}
+// } else if(document.domain.match(".advertising.com")){
+//     console.log('advertising.com');
+//     $("object param[value*='trg\%253Dhttp']").not('a[sid]').map(
+// 	function(){
+// 	    console.log("advertising.com",this);
+// 	});
+//     $("a[href*='doubleclick.net']:has(img)").not('a[sid]').map(
+// 	function(){
+// 	    console.log("advertising.com",this);
+// 	    if(m = this.href.match(/trg=(http.+)$/) && m[1]){
+// 		console.log(m[1]);
+// 	    }	
+// 	});
+// } else if(document.domain.match(".interclick.com")){
+//     $("a[href*='doubleclick.net']:has(img)").not('a[sid]').map(
+// 	function(){
+// 	    console.log("interclick.com",this);
+// 	    if(m = this.href.match(/click\.ic\%3F(http.+)$/) && m[1]){
+// 		console.log(m[1]);
+// 	    }	
+// 	});
+//     $("object param[value*='doubleclick.net']").not('a[sid]').map(
+// 	function(){
+// 	    console.log("interclick.com",this);
+// 	    if(m = this.value.match(/click\.ic\%3F(http.+)$/) && m[1]){
+// 		console.log(m[1]);
+// 	    }	
+// 	});
+// }
 
--- thinkcontext.chrome/reverse.js
+++ thinkcontext.chrome/reverse.js
@@ -1,6 +1,4 @@
 if(! document.domain.match('google.com$') || document.domain == 'news.google.com'){
-    console.log("reverse");
-
     tc.reverse = {};
     
     tc.registerResponse('link',
@@ -18,75 +16,81 @@ if(! document.domain.match('google.com$') || document.domain == 'news.google.com
 	 , key: tc.sigURL(document.baseURI)
 	});
     function doit(){
-    $("a[href*='shlinks.industrybrains.com']").not('a[sid]').map(
-	function(){
-	    var sid = "gs" + tc.random();
-	    this.setAttribute("sid",sid);
-	    if(!this.textContent.match(' ')){
-		tc.sendMessage({'kind': 'link'
-     				, 'sid': sid
-     				, 'key': tc.sigURL(this.textContent) });
-	    }
-	});
-
-    $("a[href*='googleadservices.com/pagead/aclk']").not('a[sid]').map(
-	function(){
-	    var m = this.href.match(/adurl=(http[^\&\"]+)/)
-	    var sid = "gs" + tc.random();
-	    this.setAttribute("sid",sid);
-	    if(m && m[1]){
-		tc.sendMessage({kind: 'link'
-     				, sid: sid
-     				, key: tc.sigURL(m[1]) });
-	    }
-	});
-
-    // $("object param[value*='adurl%3Dhttp%253A%252F%252Fad.doubleclick.net/click']").not('a[sid]').map(
-    // 	function(){
-    // 	    console.log(this);
-    // 	    var sid = "gs" + tc.random();
-    // 	    this.setAttribute("sid",sid);
-    // 	    var m = this.value.match(/sscs%253D%253fhttp(s)?%3A\/\/([^\/]+)/);
+	$("a[href*='shlinks.industrybrains.com']").not('a[sid]').map(
+	    function(){
+		var sid = "gs" + tc.random();
+		this.setAttribute("sid",sid);
+		if(!this.textContent.match(' ')){
+		    tc.sendMessage({'kind': 'link'
+     				    , 'sid': sid
+     				    , 'key': tc.sigURL(this.textContent) });
+		}
+	    });
+	
+	$("a[href*='googleadservices.com/pagead/aclk']").not('a[sid]').map(
+	    function(){
+		var m = this.href.match(/adurl=(http[^\&\"]+)/)
+		var sid = "gs" + tc.random();
+		this.setAttribute("sid",sid);
+		if(m && m[1]){
+		    tc.sendMessage({kind: 'link'
+     				    , sid: sid
+     				    , key: tc.sigURL(m[1]) });
+		}
+	    });
+	
+	// $("object param[name='flashvars'][value*='2mdn.net']").not('a[sid]').map(
+	//     function(){
+	// 	if(m = this.value.match(/link\%253D(http[^\&]+)/) && m[1]){
+	// 	    console.log("img 2mdn",m[1]);
+	// 	}
+	//     });
+	
+	// $("object param[value*='adurl%3Dhttp%253A%252F%252Fad.doubleclick.net/click']").not('a[sid]').map(
+	// 	function(){
+	// 	    console.log(this);
+	// 	    var sid = "gs" + tc.random();
+	// 	    this.setAttribute("sid",sid);
+	// 	    var m = this.value.match(/sscs%253D%253fhttp(s)?%3A\/\/([^\/]+)/);
     // 	    if(m && m.length == 3){
-    // 		console.log('doubleclick param ' + m[2]);
-    // 		tc.sendMessage({kind: 'link'
-    // 				, subtype: 'imgad'
-    // 				, sid: sid
-    // 				, key: tc.sigURL(m[2])});
-				
-						 
-    // 	    }
-    // 	});
-
-    // $("a[href*='adurl%3Dhttp%253A%252F%252Fad.doubleclick.net/click']:has(img)").not('a[sid]').map(
-    // 	function(){
-    // 	    var sid = "gs" + tc.random();
-    // 	    this.setAttribute("sid",sid);
-    // 	    var m = this.value.match(/sscs%253D%253fhttp(s)?%3A\/\/([^\/]+)/);
-    // 	    if(m && m.length == 3){
-    // 		console.log('doubleclick param ' + m[2]);
-    // 		tc.sendMessage({kind: 'link'
-    // 				, subtype: 'imgad'
-    // 				, sid: sid
-    // 				, key: tc.sigURL(m[2])});
-				
-						 
-    // 	    }
-    // 	});
-
-    // $("object[flashvars*='click=http%3A%2F%2Fad.doubleclick.net']").not('a[sid]').map(
-    // 	function(){
-    // 	    var m = this.attributes.flashvars.textContent.match(/exitEvents=[^\&]*url%253Ahttp%25253A%2F%2F([^\%]+)/);
-    // 	    if(m && m.length == 2){
-    // 		console.log('doubleclick object ' + m[1]);
-    // 		tc.sendMessage({kind: 'link'
-    // 				, subtype: 'imgad'
-    // 				, sid: sid
-    // 				, key: tc.sigURL(m[2])});		
-    // 	    }
-    // 	});
+	// 		console.log('doubleclick param ' + m[2]);
+	// 		tc.sendMessage({kind: 'link'
+	// 				, subtype: 'imgad'
+	// 				, sid: sid
+	// 				, key: tc.sigURL(m[2])});
+	
+	
+	// 	    }
+	// 	});
+	
+	// $("a[href*='adurl%3Dhttp%253A%252F%252Fad.doubleclick.net/click']:has(img)").not('a[sid]').map(
+	// 	function(){
+	// 	    var sid = "gs" + tc.random();
+	// 	    this.setAttribute("sid",sid);
+	// 	    var m = this.value.match(/sscs%253D%253fhttp(s)?%3A\/\/([^\/]+)/);
+	// 	    if(m && m.length == 3){
+	// 		console.log('doubleclick param ' + m[2]);
+	// 		tc.sendMessage({kind: 'link'
+	// 				, subtype: 'imgad'
+	// 				, sid: sid
+	// 				, key: tc.sigURL(m[2])});
+	
+	
+	// 	    }
+	// 	});
+	
+	// $("object[flashvars*='click=http%3A%2F%2Fad.doubleclick.net']").not('a[sid]').map(
+	// 	function(){
+	// 	    var m = this.attributes.flashvars.textContent.match(/exitEvents=[^\&]*url%253Ahttp%25253A%2F%2F([^\%]+)/);
+	// 	    if(m && m.length == 2){
+	// 		console.log('doubleclick object ' + m[1]);
+	// 		tc.sendMessage({kind: 'link'
+	// 				, subtype: 'imgad'
+	// 				, sid: sid
+	// 				, key: tc.sigURL(m[2])});		
+	// 	    }
+	// 	});
     }
-
-    window.setTimeout(doit,500);
     
+    window.setTimeout(doit,500);
 }
