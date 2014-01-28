ee5e9b585e74293650d7e705324b6efd275f7e80
thinkContext <contact@thinkcontext.org>
2014-01-28 18:45:18 -0500

    minor

--- chrome-ydb-db/background.js
+++ chrome-ydb-db/background.js
@@ -1,22 +1,38 @@
-tc = {};
-
-tc.thing_schema = {
-    name: 'thing'
-    , indexes: [
-	{ keyPath: 'handles'
-	  , multiEntry: true
-	}
-    ]
+var tc = {
+    schema: { 
+	stores: [
+	    {
+		name: 'thing'
+		, keyPath: '_id'
+		, indexes: [
+		    { keyPath: 'handles'
+		      , multiEntry: true
+		    }
+		]
+	    }
+	]
+	, version: 7
+    }
+    , dbName: 'tc'
+    , db: new ydn.db.Storage(this.dbName,this.schema)
+    , dataUrl: 'http://127.0.0.1:5984/tc/_design/think/_view'
+    
+    , load: function(){
+	var _self = this;
+	console.log('load');
+	$.getJSON(this.dataUrl + '/dataByCampaign'
+		  ,function(data){
+		      console.log(data);
+		      var req, rows = data.rows.map(function(x){return x.value;});		      
+		      if(rows.length > 0){
+			  req = _self.db.put('thing',rows);
+			  req.done(function(key) {
+			      console.log(key);
+			  });
+			  req.fail(function(e) {
+			      throw e;
+			  });
+		      }
+		  });
+    }
 };
-
-tc.template_schema = {
-    name: 'template'
-    , keyPath: 'campaign'
-};
-
-tc.schema = { 
-    stores: [ tc.thing_schema, tc.template_schema ]
-    , version: 1
-};
-
-tc.db = new ydn.db.Storage('tc',tc.schema);
