28174e37a0f3b10da20045ecbfeda41340a44a12
thinkcontext <contact@thinkcontext.org>
2014-02-26 07:24:41 -0500

    dev sync

---
+++ chrome-ydb-db/bing-search.js
@@ -0,0 +1,9 @@
+// result link
+tc.searchLinkExam('ol#b_results li.b_algo h2 a','bing-search');
+
+// ads
+tc.searchLinkExam('li.b_ad div.sb_add:has(h2 > a) div.b_caption > div.b_attribution > cite'
+		  ,'bing-search'
+		  , function(x){ return x.parentElement.parentElement.parentElement.children[0].children[0];}
+		  , function(x){return x.textContent;});
+
