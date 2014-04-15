66bfa77ef55f99854ad22119d022ded2c74842d7
thinkcontext <contact@thinkcontext.org>
2014-04-15 07:58:03 -0400

    minor

--- chrome/options.js
+++ chrome/options.js
@@ -1,3 +1,13 @@
 var bgPage = chrome.extension.getBackgroundPage();
 var campaigns = bgPage.tc.campaigns;
 var actions = bgPage.tc.actions;
+var campaign;
+
+for(var i in campaigns){
+    campaign = campaigns[i];
+    $("<tr>")
+	.append($("<td>",{text: campaign['tid']}))
+	.append($("<td>",{text: campaign['title']}))
+	.appendTo("#campaigns");
+    
+}
