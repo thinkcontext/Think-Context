8444dd82b09c46b9516e33ba156d70f2cb5e218c
thinkcontext <contact@thinkcontext.org>
2014-04-17 00:24:34 -0400

    yelp, fb mutation summary

--- chrome/facebook.js
+++ chrome/facebook.js
@@ -1,8 +1,33 @@
+tc.facebook = {};
+var $observerSummaryRoot = $("body");
+
+tc.facebook.summaryCallback = function(summaries){
+    $observerSummaryRoot.mutationSummary("disconnect");
+    tc.facebook.doOb();
+}
+
+tc.facebook.doOb = function(){
+    tc.facebook.examine();
+    window.setTimeout(tc.facebook.observe,500);
+    window.setTimeout(tc.facebook.examine,1000);
+}
+
+tc.facebook.observe = function(){
+    $observerSummaryRoot.mutationSummary("connect"
+					 , tc.facebook.summaryCallback
+					 , [{ element:"div" }]);
+}
+
+tc.facebook.examine = function(){
+    tc.handleExamine("div.ego_unit > div:first-child > div:first-child > a:nth-child(2) div[title] > div:nth-child(2)"
+		     ,null
+		     ,function(x){ return x.textContent }
+		    );
+    
+    tc.handleExamine("div:has(a.uiStreamSponsoredLink) > h5 a");
+});}
+
+tc.facebook.doOb();
+
 tc.popSend();
 
-tc.handleExamine("div.ego_unit > div:first-child > div:first-child > a:nth-child(2) div[title] > div:nth-child(2)"
-		 ,null
-		 ,function(x){ return x.textContent }
-		);
-
-tc.handleExamine("div:has(a.uiStreamSponsoredLink) > h5 a");
--- chrome/yelp.js
+++ chrome/yelp.js
@@ -1,3 +1,27 @@
 console.log('yelp');
 tc.popSend();
-tc.handleExamine('a.biz-name','yelp');
+tc.yelp = {};
+var $observerSummaryRoot = $("body");
+
+tc.yelp.summaryCallback = function(summaries){
+    $observerSummaryRoot.mutationSummary("disconnect");
+    tc.yelp.doOb();
+}
+
+tc.yelp.doOb = function(){
+    tc.yelp.examine();
+    window.setTimeout(tc.yelp.observe,500);
+    window.setTimeout(tc.yelp.examine,1000);
+}
+
+tc.yelp.observe = function(){
+    $observerSummaryRoot.mutationSummary("connect"
+					 , tc.yelp.summaryCallback
+					 , [{ element:"div" }]);
+}
+
+tc.yelp.examine = function(){
+    tc.handleExamine('a.biz-name','yelp');
+};
+
+tc.yelp.doOb();
